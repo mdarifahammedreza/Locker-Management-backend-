0e306d313a005249ee106aba62a81830fc3f562b
mdarifahammedreza <mdarifahammedreza@gmail.com>
2024-11-09 11:25:05 -0800

    api checked

--- Status.js
+++ Status.js
@@ -1,6 +1,6 @@
 // Status.js
 const status = {
-  Status_code: 400,
+  Status_code: 200,
   Status: "Locker server run successfully",
   Author: "Md Arif Ahammed Reza",
   Contact: "reza35-951@diu.edu.bd"
